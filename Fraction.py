54e0885cb2d6fa3056c6802f9b090fac902eb4b8
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2025-02-03 22:02:02 +0800

    [Fraction] added code to main

--- Fraction.py
+++ Fraction.py
@@ -1,21 +1,71 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO
+        
+        if isinstance(numerator, str):
+            try:
+                tempValues = numerator.split("/", 1)
+                self.numerator = int(tempValues[0])
+                self.denominator = int(tempValues[1])
+            except ValueError:
+                self.fraction_string = '0'
+                return
+                
+        elif isinstance(numerator, float) or isinstance(denominator, float):
+            self.fraction_string = '0'
+            return
+
+        elif isinstance(numerator, int) and isinstance(denominator, int):
+            self.numerator = numerator
+            self.denominator = denominator
+
+        if self.denominator < 0:
+            self.numerator *= -1
+            self.denominator *= -1
+            
+        if self.denominator == 0:
+            raise ZeroDivisionError("ZeroDivisionError: Cannot divide by zero.")
+        
+        elif self.numerator == 0:
+            self.fraction_string = '0'
+            return
+
+        else:
+            self.divisor = self.gcd(self.numerator, self.denominator)
+            self.fraction_string = str(int(self.numerator/self.divisor)) + '/' + str(int(self.denominator/self.divisor))
+       
         pass
 
+    @staticmethod
     def gcd(a, b):
         #TODO
-        pass
+        #''
+        # '
+      
+        a = abs(a)
+        b = abs(b)
+        
+        if b == 0 or a == 0:
+            return 0
+        
+        while b != a:
+            if b > a:
+                b = b - a
+            if a > b:
+                a = a - b
+        return a
 
     def get_numerator(self):
-        #TODO
+      
+        return numerator
         pass
 
     def get_denominator(self):
-        #TODO
+       
+        return denominator
         pass
 
     def get_fraction(self):
-        #TODO
+        return self.fraction_string
         pass
+
